2c9a68d566732e2ca05fe54a397afa5b6369d79b
Matthew Kaufer <mjkaufer@gmail.com>
2018-12-29 16:55:31 -0500

    Better flocking

--- js/script.js
+++ js/script.js
@@ -1,130 +1,35 @@
-console.log(paper)
-window.project = project
-// Ported from original Metaball script by SATO Hiroyuki
-// http://park12.wakwak.com/~shp/lc/et/en_aics_script.html
+var width = project.view.size.width
+var height = project.view.size.height
+
 project.currentStyle = {
     fillColor: 'black'
 };
 
-var radiusMin = 30;
-var radiusMax = 40;
+var radiusMin = 10;
+var radiusMax = 15;
 var eyeDistance = radiusMin * 0.8;
 var defaultPadding = 20 + radiusMax;
 var currentHue = Math.random()
 var bodyConnections = new Group();
-var gridCenter = new Point(project.view.size / 2)
-console.log(gridCenter)
-function getHue(){
-    return "hsl(" + currentHue * 360 + ", 50, 50)"
-}
 
-function randomPoint(padding) {
-    if (!padding) {
-        padding = defaultPadding
-    }
-    var x = (project._view.size._width - padding) * Math.random() + padding / 2;
-    var y = (project._view.size._height - padding) * Math.random() + padding / 2;
-    return new Point(x, y)
-}
-
+var goals = []
 var goalLocation = randomPoint(defaultPadding * 4)
-var goal = new Path.Circle({
-    center: goalLocation,
-    radius: radiusMin / 2,
-    fillColor: "black"
-})
 
-function makeMonster(x, y, radius) {
-    if (!x || !y) {
-        var p = randomPoint()
-        x = p.x
-        y = p.y
-    }
-    if (!radius) {
-        radius = Math.random() * (radiusMax - radiusMin) + radiusMin;
-    }
+var goal = makeGoal(goalLocation.x, goalLocation.y, repulseThreshold, "black")
+makeGoal(width / 4, height / 4, repulseThreshold * 0.5)
+makeGoal(width * 3 / 4, height / 4, repulseThreshold * 0.5)
+makeGoal(width * 3 / 4, height * 3 / 4, repulseThreshold * 0.5)
+makeGoal(width / 4, height * 3 / 4, repulseThreshold * 0.5)
 
-    var orientation = Math.random() * 360;
-    var eyeRadius = radius / 6
-
-    var center = new Point(x, y);
-    var leftEyeCenter = new Point(0, eyeDistance)
-    leftEyeCenter.angle = orientation + 30
-    var rightEyeCenter = new Point(0, eyeDistance)
-    rightEyeCenter.angle = orientation - 30
-
-    var body = new Path.Circle({
-        center: center,
-        radius: radius,
-        fillColor: getHue()
-    })
-    var group = new Group();
-
-    var pseudoBody = new Path.Circle({
-        center: center,
-        radius: radius,
-        fillColor: "transparent"
-    })
-
-    var leftEye = new Path.Circle({
-        center: leftEyeCenter + center,
-        radius: eyeRadius,
-        fillColor: "white"
-    })
-    var rightEye = new Path.Circle({
-        center: rightEyeCenter + center,
-        radius: eyeRadius,
-        fillColor: "white"
-    })
-
-    group.appendBottom(pseudoBody)
-    group.appendTop(leftEye)
-    group.appendTop(rightEye)
-
-    return {
-        flee: false,
-        group: group,
-        body: body,
-        leftEye: leftEye,
-        rightEye: rightEye,
-        fleeCooldown: 200,
-        radius: radius,
-        // velocity: Point.random() * 2 - new Point(1, 1),
-        velocity: getVectorDeg(Math.random() * 360, 1),
-        updateFlee: function() {
-            var originalFlee = this.flee
-
-            if (this.fleeCooldown <= 0) {
-                if ((group.position - goal.position).length < 150) {
-                    this.flee = true
-                } else {
-                    this.flee = false
-                }
-                this.fleeCooldown = 200
-            } else {
-                this.fleeCooldown -= 1
-            }
-            
-        },
-        move: function(velocity) {
-            this.body.position += velocity
-            this.group.position += velocity
-            var angle = Math.atan2(velocity.y, velocity.x) * 180 / Math.PI
-
-            this.leftEye.position = this.group.position + getVectorDeg(angle + 30, eyeDistance)
-            this.rightEye.position = this.group.position + getVectorDeg(angle - 30, eyeDistance)
-            this.velocity = velocity
-        }
-    };
-}
 
 var handle_len_rate = 2.4;
 var bodys = [];
 var groups = [];
 var eyes = [];
 var monsters = [];
-var radius = 50;
-for (var i = 0; i < 10; i++) {
+var numMonsters = 30
+
+for (var i = 0; i < numMonsters; i++) {
     var monster = makeMonster();
     bodys.push(monster.body);
     groups.push(monster.group);
@@ -132,117 +37,125 @@ for (var i = 0; i < 10; i++) {
     eyes.push(monster.rightEye);
     monsters.push(monster);
 }
+
 var eyeConnections = new Group();
 
-window.bodys = bodys
-window.eyes = eyes
-window.monsters = monsters
-var mouseMonster = monsters[0]
-
-function resetBacks() {
-    monsters.forEach(function(monster) {
-        mouseMonster.body.sendToBack()
-    })
-}
+var neighborhoodSize = radiusMax * 5
+var repulseThreshold = neighborhoodSize
+var minSpacing = radiusMax * 1.5
+var learnRate = 0.05
 
 function tick() {
-    currentHue = (currentHue + 0.01) % 1
+    currentHue = (currentHue + 0.0005) % 1
     monsters.forEach(function(monster, monsterIndex){
-        var repulse = new Point(0, 0)
 
-        monsters.forEach(function(otherMonster, otherMonsterIndex) {
+        var nearbyMonsters = monsters.filter(function(otherMonster, otherMonsterIndex) {
             if (monsterIndex == otherMonsterIndex) {
-                return
+                return false
+            } else if ((monster.body.position - otherMonster.body.position).length < neighborhoodSize) {
+                return true
             } else {
-                var ratio = Math.pow(otherMonster.radius / monster.radius, 2)
-                // var ratio = 1
-                var currentRepulse = (monster.body.position - otherMonster.body.position) * ratio
-                if (currentRepulse.length > radiusMax * 2.5) {
-                    currentRepulse /= Math.pow(currentRepulse.length / 5, 2)
-                    repulse += currentRepulse
-                }
-                
+                return false
             }
         })
 
-        var corners = [new Point(view.size.width / 2, 0), new Point(view.size.width, view.size.height / 2), new Point(0, view.size.height / 2), new Point(view.size.width / 2, view.size.height)]
-        corners.forEach(function(corner, i) {
-            var d = monster.group.position - corner
-            var n = gridCenter - corner
-            var dist = d.dot(n) / n.length
-            var cornerRepulse = (gridCenter - monster.group.position)
-            if (dist < defaultPadding) {
-                repulse = cornerRepulse * 2000
+        // align to neighbors
+        var alignment = getVectorDeg(monster.orientation, 1)
+
+        nearbyMonsters.forEach(function(nearbyMonster) {
+            alignment += getVectorDeg(nearbyMonster.orientation, 1)
+        })
+
+        if (nearbyMonsters.length > 0) {
+            alignment /= (nearbyMonsters.length + 1)
+        }
+
+        // move away from neighbors too close, move to center
+        var repulsion = new Point(0, 0)
+        var center = new Point(0, 0)
+        var repelling = false
+        nearbyMonsters.forEach(function(nearbyMonster) {
+            var delta = nearbyMonster.body.position - monster.body.position
+            // move away
+            if (delta.length < minSpacing) {
+                var magnitude = 1 - (delta.length / minSpacing)
+                repulsion -= delta.normalize() * magnitude
+                repelling = true
             } else {
-                repulse += (cornerRepulse) / cornerRepulse.length / 20
+                // move to center
+                center = center + delta
             }
         })
 
-        monster.updateFlee()
-        var delta = (goal.position - monster.group.position) * (monster.flee ? -1 : 1)
-        // var delta = goal.position - monster.group.position
-        delta = delta / delta.length
-        // var newVelocity = monster.velocity * 0.995
-        // newVelocity += delta * 0.004
-        // newVelocity += getVectorDeg(Math.random() * 360, 0.001)
-        // monster.move(newVelocity)
-        // monster.velocity = newVelocity
-        var newVelocity = new Point(0, 0)
-        newVelocity += repulse * 0.3
-        newVelocity += monster.velocity * 0.9
-        newVelocity += delta * 0.1
-        newVelocity += getVectorDeg(Math.random() * 360, 0.001) * 0.125
-        newVelocity /= newVelocity.length
-        monster.move(newVelocity)
-        // if (monster.flee) {
-        //     monster.body.fillColor = "red"
-        // } else {
-        //     monster.body.fillColor = "blue"
-        // }
-        monster.body.fillColor = getHue()
+
+        if (nearbyMonsters.length > 0) {
+            alignment /= (nearbyMonsters.length + 1)
+            center /= (nearbyMonsters.length)
+            center = center.normalize() / 100
+        }
+
+        var obstacleRepulsion = new Point(0, 0)
+
+        for (var i = 0; i < goals.length; i++) {
+            var currentGoal = goals[i]
+            var obstacleDelta = monster.body.position - currentGoal.position
+            var repulseDistance = obstacleDelta.length
+            if (repulseDistance <= repulseThreshold) {
+                var beta = 1 - (repulseDistance / repulseThreshold)
+
+                monster.magnitude = Math.max(0.75 + 0.1 * monster.magnitude + 1.25 * Math.sqrt(beta), 1)
+                obstacleRepulsion += obstacleDelta
+            } else if (monster.magnitude > 1) {
+                monster.magnitude *= 0.95
+            }
+        }
+        
+
+        var random = getVectorDeg(Math.random() * 360, 1) / 100
+
+        var direction = random + alignment + repulsion + obstacleRepulsion + center
+
+        monster.newOrientation = direction.angle * learnRate + monster.orientation * (1 - learnRate)
+
+        monster.body.fillColor = getHue(monster)
+    })
+
+    monsters.forEach(function(monster) {
+        monster.orientation = monster.newOrientation
+        monster.move()
+        monster.body.position.x = (monster.body.position.x + 2 * width) % width
+        monster.body.position.y = (monster.body.position.y + 2 * height) % height
+        monster.group.position.x = (monster.group.position.x + 2 * width) % width
+        monster.group.position.y = (monster.group.position.y + 2 * height) % height
     })
 
 
     generateConnections(bodys, false);
-
     generateConnections(eyes, true);
-    resetBacks()
 
     monsters.forEach(function(monster){
         project.activeLayer.appendTop(monster.group)
     })
 
-    goal.sendToBack()
 }
 
+tick()
 setInterval(tick, 20)
 
-
 function onMouseMove(event) {
-
     goal.position = event.point
-    // mouseMonster.body.sendToBack()
-    // monsters[monsters.length - 1].group.sendToBack()
-    
-
-
 }
-tick()
-resetBacks()
 
 function generateConnections(paths, isEye) {
     var connections = bodyConnections
-    var maxRadius = 150
+    var maxRadius = 50
     var ratio = 0.5
     if (isEye) {
         connections = eyeConnections
-        maxRadius = 20
+        maxRadius = 5
         ratio = 0.7
     }
-    // connections.children.forEach(function(child) {
-    //     child.remove()
-    // })
-    // console.log(connections.children.length)
+
     connections.removeChildren()
     // Remove the last connection paths:
     connections.children = [];
@@ -255,17 +168,12 @@ function generateConnections(paths, isEye) {
                     connections.appendTop(path);
                 } else {
                     connections.appendBottom(path);    
-                    // path.sendToBack()
                 }
-                
-                // path.removeOnMove();
             }
         }
     }
 }
 
-// onMouseMove()
-// ---------------------------------------------
 function metaball(ball1, ball2, v, handle_len_rate, maxDistance, isEye) {
     var center1 = ball1.position;
     var center2 = ball2.position;
@@ -330,13 +238,105 @@ function getVector(radians, length) {
     return new Point({
         // Convert radians to degrees:
         angle: radians * 180 / Math.PI,
-        length: length
+        length: (length || 1)
     });
 }
 function getVectorDeg(degrees, length) {
     return new Point({
         // Convert radians to degrees:
         angle: degrees,
-        length: length
+        length: (length || 1)
     });
+}
+function getHue(monster){
+    var l = 50
+    if (monster) {
+        l *= (1 / monster.magnitude)
+    }
+    return "hsl(" + currentHue * 360 + ", 50, " + l + ")"
+}
+
+function randomPoint(padding) {
+    if (!padding) {
+        padding = defaultPadding
+    }
+    var x = (project._view.size._width - padding) * Math.random() + padding / 2;
+    var y = (project._view.size._height - padding) * Math.random() + padding / 2;
+    return new Point(x, y)
+}
+
+function makeMonster(x, y, radius) {
+    if (!x || !y) {
+        var p = randomPoint()
+        x = p.x
+        y = p.y
+    }
+    if (!radius) {
+        radius = Math.random() * (radiusMax - radiusMin) + radiusMin;
+    }
+
+    var orientation = Math.random() * 360;
+    var eyeRadius = radius / 6
+
+    var center = new Point(x, y);
+    var leftEyeCenter = new Point(0, eyeDistance)
+    leftEyeCenter.angle = orientation + 30
+    var rightEyeCenter = new Point(0, eyeDistance)
+    rightEyeCenter.angle = orientation - 30
+
+    var body = new Path.Circle({
+        center: center,
+        radius: radius,
+        fillColor: getHue()
+    })
+    var group = new Group();
+
+    var pseudoBody = new Path.Circle({
+        center: center,
+        radius: radius,
+        fillColor: "transparent"
+    })
+
+    var leftEye = new Path.Circle({
+        center: leftEyeCenter + center,
+        radius: eyeRadius,
+        fillColor: "white"
+    })
+    var rightEye = new Path.Circle({
+        center: rightEyeCenter + center,
+        radius: eyeRadius,
+        fillColor: "white"
+    })
+
+    group.appendBottom(pseudoBody)
+    group.appendTop(leftEye)
+    group.appendTop(rightEye)
+    return {
+        group: group,
+        body: body,
+        leftEye: leftEye,
+        rightEye: rightEye,
+        radius: radius,
+        orientation: orientation,
+        magnitude: 1,
+        move: function() {
+            var velocity = getVectorDeg(this.orientation, this.magnitude)
+            this.body.position += velocity
+            this.group.position += velocity
+
+            this.leftEye.position = this.group.position + getVectorDeg(this.orientation + 30, eyeDistance)
+            this.rightEye.position = this.group.position + getVectorDeg(this.orientation - 30, eyeDistance)
+        }
+    };
+}
+
+function makeGoal(x, y, power, color) {
+    var newGoal = new Path.Circle({
+        center: new Point(x, y),
+        radius: radiusMin / 2,
+        fillColor: color || "#777"
+    })
+    newGoal.power = (power || radiusMax * 2)
+    goals.push(newGoal)
+    return newGoal
 }
